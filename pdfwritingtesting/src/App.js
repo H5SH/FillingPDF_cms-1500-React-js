e7d5d6342906584b3b735e2abffd7aa0ba89cad5
Hasham Asad <tipuasad2001@gmail.com>
2024-01-18 21:16:06 +0500

    requiredFileLoaded

--- pdfwritingtesting/src/App.js
+++ pdfwritingtesting/src/App.js
@@ -2,12 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import { PDFViewer } from '@react-pdf/renderer';
 import MyDocument from './pdf/MyDocument';
+import {Document, Page} from 'react-pdf'
+import { useState } from 'react';
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
 
 function App() {
+
+  
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }){
+    setNumPages(pageNumber);
+  }
+
   return (
-    <PDFViewer>
-      <MyDocument />
-    </PDFViewer>
+    <div>
+      <Document file='https://www.cigna.com/static/www-cigna-com/docs/form-cms1500.pdf'
+      onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={numPages}/>
+      </Document>
+    </div>
+    // <PDFViewer>
+    //   <Form />
+    // </PDFViewer>
   );
 }
 
