327b372ac4a400ce6e0dcc2ea315465c5dbec64a
Hasham Asad <tipuasad2001@gmail.com>
2024-01-19 21:32:02 +0500

    formFilledWithItsFieldNameUsingLoop

--- pdfwritingtesting/src/App.js
+++ pdfwritingtesting/src/App.js
@@ -2,14 +2,22 @@ import {Document, Page, pdfjs} from 'react-pdf'
 import { useEffect, useState } from 'react';
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib'
 import { getPdfFormCigna } from './__request/getPdf';
-import ReactPDF from '@react-pdf/renderer';
+import { saveAs } from 'file-saver'
+import { StyleSheet } from '@react-pdf/renderer'
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
+
+// To display the pdf on Browser
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
   import.meta.url,
 ).toString();
 
-const pdfform = require('./pdfform.js-master/pdfform')
+const styles = StyleSheet.create({
+  page: {backgroundColor: 'tomato'},
+  section: {color: 'white', textAlign: 'center', margin: 30, height: 40}
+})
 
 
 function App() {
@@ -35,11 +43,125 @@ function App() {
    
     const form = pdfDoc.getForm()
     form.updateFieldAppearances()
-    form.getFields().find(x=>console.log(x.getName()))
-    const pt_cityField = form.getTextField("pt_city")
-    pt_cityField.setText("Lahore")
-    // const nameField = form.getTextField("CITY")
-    // const checkbox = form.getCheckBox("Self")
+
+    // to display all the fields name in console
+    form.getFields().find(x=>{
+      const field = form.getFieldMaybe(x.getName())
+      if(field){
+        try{
+          const checkboxes = [
+            '276',
+            'sex',
+            'rel_to_ins' ,
+            'ins_sex' ,
+            'ins_benefir_plan' ,
+            'lab' ,
+            'assignment' ,
+            'employement' , 
+            'pt_auto_accident' ,
+            'other_accident' ,
+            'ssn' ,
+            'insurance_type' ,
+            'ins_benefit_plan' ,
+            'employment'
+          ]
+          if(checkboxes.includes(x.getName())){
+            form.getCheckBox(x.getName()).check()
+          }else if(x.getName() === "Clear Form"){
+            form.getButton(x.getName())
+          }else{
+            const length = form.getTextField(x.getName()).getMaxLength()
+            if(length === undefined || length > 4){
+              form.getTextField(x.getName()).setText(x.getName())
+            }else{
+              form.getTextField(x.getName()).setText(x.getName().slice(0, length))
+            }
+          }
+
+        }catch(err){
+          console.log("LOOP ERROR")
+          console.log(err)
+        }
+      }
+    })
+
+    // // Main Title of the Insurance no visible field
+    // const mainTitle = form.getTextField('insurance_name')
+    // mainTitle.setText('insurance_name')
+
+    // // 8. RESERVED FOR NUCC USE 
+    // const reserverdForNuccUse_8 = form.getTextField('NUCC USE')
+    // reserverdForNuccUse_8.setText('NUCC USE')
+
+    // // b. RESERVED FOR NUCC USE
+    // const reserverdForNuccUse_b = form.getTextField('40')
+    // reserverdForNuccUse_b.setText('40')
+
+    // // c. RESERVED FOR NUCC USE
+    // const reserverdForNuccUse_c = form.getTextField('41')
+    // reserverdForNuccUse_c.setText('41')
+
+    // // 10d. CLAIM CODES (Designated by NUCC)
+    // const claimCodes_10d = form.getTextField('50')
+    // claimCodes_10d.setText('50')
+
+    // // fields under b. OTHER CLAIM ID (Designated by NUCC)
+    // // left field 
+    // const otherClaimId_b_left = form.getTextField('57')
+    // otherClaimId_b_left.setText('57')
+    // // right field  
+    // const otherClaimId_b_right = form.getTextField('58')
+    // otherClaimId_b_right.setText('58')
+
+    // // fields under 14. DATE OF CURRENT ILLNESS, INJURY, or PReGNANCY (LMP)
+    // // right most
+    // const dateOfCurrentIllness_14 = form.getTextField('73')
+    // dateOfCurrentIllness_14.setText('73')
+
+    // // fields under 15. OTHER DATE
+    // // QUAL
+    // const otherDate_15_qual = form.getTextField('74')
+    // otherDate_15_qual.setText('74')
+    
+    // // fields under 17.NAME OF REFERRING PROVIDER OR OTHER SOURCE
+    // // left field
+    // const nameOfReferringProvider_17_left = form.getTextField('85')
+    // nameOfReferringProvider_17_left.setText('85')  
+
+    // // 19. ADDITIONAL CLAIM INFORMATION (Designated by NUCC)
+    // const additionalClaimInformation_19 = form.getTextField('96')
+    // additionalClaimInformation_19.setText("96")
+
+    // // fields under 21. DIAGNOSIS OR NATURE OF ILLNESS OR INJURY Relate A-L to service line below ()
+    // // ICD_Ind
+    // const icd_ind =  form.getTextField('99icd')
+    // icd_ind.setText('99icd')
+
+    // form.getTextField('135').setText('135')
+    // form.getTextField('157').setText('157')
+    // form.getTextField('179').setText('179')
+    // form.getTextField('201').setText('201')
+    // form.getTextField('223').setText('223')
+    // form.getTextField('245').setText('245')
+
+    // // 28.TOTAL CHARGE
+    // const totalCharge_28 = form.getCheckBox('276')
+    // totalCharge_28.check()
+
+    // // Button
+    // form.getButton('Clear Form')
+
+
+    
+
+    // // CITY
+    // const city = form.getTextField("pt_city")
+    // city.setText("Lahore")
+
+    // // fields under 16. DATES PATIENT UNABLE TO WORK IN CURRENT OCCUPATION
+    // form.getTextField('work_yy_from').setText('22')
+    
+    
 
     // const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica) 
 
@@ -66,6 +188,8 @@ function App() {
     const pdfBytes = await pdfDoc.save()
     const file = new Blob([pdfBytes], {type:'applocation/pdf'})
     setPdf(URL.createObjectURL(file))
+    // to save the file
+    //saveAs(pdf, `Filled-cms-1500.pdf`)
     // ReactPDF.render(file, `form-cms${count++}.pdf`)
   }catch(err){
     console.log(err)
@@ -74,7 +198,6 @@ function App() {
   }
 
   useEffect(()=>{
-    console.log("function")
     WriteOnPdf()
   },[])
   
@@ -82,9 +205,13 @@ function App() {
   return (
     <div>
     {response ? <p>{response}</p>:
-      <Document file={pdf}>
-        <Page size={'A4'} pageNumber={1}/>
+    <>
+      <Document file={pdf} style={styles.section}>
+      <a href={pdf} download={'Filled-cms-1500.pdf'} target='C:/H5SH/Work/FillingPDF_cms-1500-React-js'>DownLoad</a>
+        <Page size={'A4'} pageNumber={1} style={styles.page}/>
       </Document>
+      
+      </>
 }
       
        {/* <PDFViewer>
